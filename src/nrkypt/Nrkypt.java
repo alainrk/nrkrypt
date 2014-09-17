1cdadc1f4908573038c07518d6ec16d8a1ccd608
alainrk <alain.narko@gmail.com>
2014-09-17 20:42:50 +0200

    Just trying

--- src/nrkypt/Nrkypt.java
+++ src/nrkypt/Nrkypt.java
@@ -4,6 +4,20 @@
  */
 package nrkypt;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import org.bouncycastle.crypto.BlockCipher;
+import org.bouncycastle.crypto.engines.AESEngine;
+import org.bouncycastle.crypto.modes.CBCBlockCipher;
+import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
+
+import javax.crypto.*;
+import java.security.*;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  *
  * @author narko
@@ -13,7 +27,48 @@ public class Nrkypt {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException {
+        //add at runtime the Bouncy Castle Provider
+    	//the provider is available only for this application
+    	Security.addProvider(new BouncyCastleProvider());
+ 
+    	//BC is the ID for the Bouncy Castle provider;
+        if (Security.getProvider("BC") == null){
+            System.out.println("Bouncy Castle provider is NOT available");
+        }
+        else{
+            System.out.println("Bouncy Castle provider is available");
+        }
+        
+        BlockCipher engine = new AESEngine();
+        PaddedBufferedBlockCipher encryptCipher;
+        encryptCipher = new PaddedBufferedBlockCipher(
+		new CBCBlockCipher(engine)
+                );
+        
+        
+        String pass = "b";
+        
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] result = mDigest.digest(pass.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        
+        String hash = sb.toString();
+        //System.out.println(hash.getBytes().length + " " + sb.toString());
+        
+        byte[] hashbytes = sb.toString().getBytes();
+        byte[] key = new byte[(hashbytes.length / 2) - 4];
+        
+        for(int i = 0; i < key.length; i++){
+            // using ALL of the 256 bits generated
+            key[i] = (byte) (hashbytes[i] ^ hashbytes[i + key.length]); //TODO: Last 4 bytes not used
+        }
+
+        SecretKeySpec AESkey = new SecretKeySpec(key, "AES");
+        Cipher c = Cipher.getInstance("AES");
+        c.init(Cipher.SECRET_KEY,AESkey);
     }
 }
