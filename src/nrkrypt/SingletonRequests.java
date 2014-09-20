42e5f981de21fd5922cae4dcdaf9587393e732ed
alainrk <alain.narko@gmail.com>
2014-09-20 12:05:53 +0200

    Modifying singleton

--- src/nrkrypt/SingletonRequests.java
+++ src/nrkrypt/SingletonRequests.java
@@ -18,8 +18,7 @@ import org.xml.sax.SAXException;
  * @author narko
  */
 public class SingletonRequests {
-    //private DocumentBuilderFactory dictDOMFactory;
-    /* Name and corrispondent DOMDocument for DOM access dictNameMapping.xml */
+
     private Document XML_MAP_DOC = null;
     private String CURRENT_PLAINDB_NAME = "";
     private Document CURRENT_PLAINDB_DOC = null;
@@ -42,19 +41,26 @@ public class SingletonRequests {
             Logger.getLogger(SingletonRequests.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    /* Load in memory (see "currentDict" variable) a dictionary */
-    public void loadDict (String dict) {
-        CURRENT_PLAINDB_NAME = dict+".xml";
+
+    public void loadUser (String user) {
+        CURRENT_PLAINDB_NAME = user+".xml";
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         try {
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
             CURRENT_PLAINDB_DOC = docBuilder.parse(CURRENT_PLAINDB_NAME);
-            System.out.println("DEBUGGING: loadDict, dictionary loaded: "+dict);
+            System.out.println("DEBUGGING: loadUser, user loaded: "+user);
         } catch (ParserConfigurationException | SAXException | IOException ex) {
             Logger.getLogger(SingletonRequests.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    
+    
+    
+    
+    
+    
+    
     
     public String getTransl (String word){
         NodeList nodes = getNodeListFromDoc(CURRENT_PLAINDB_DOC, "/terms/term/word[text() = '"+word+"']/../transl");
@@ -87,7 +93,7 @@ public class SingletonRequests {
         return words;
     }
     
-    /* Create new term in current dictionary, on error return -1 */
+    /* Create new term in current userDB, on error return -1 */
     public int createTerm (String word, String transl){
         // TODO: Check if already exist (USE XPATH ID OR SIMILAR IF POSSIBLE
         NodeList nodes = getNodeListFromDoc(CURRENT_PLAINDB_DOC, "/terms");
@@ -143,63 +149,78 @@ public class SingletonRequests {
     }
     
     
-    public ArrayList<String> getAllDicts (){
-        NodeList nodes = getNodeListFromDoc(XML_MAP_DOC, "/dicts/dict/name");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public ArrayList<String> getAllUsers (){
+        NodeList nodes = getNodeListFromDoc(XML_MAP_DOC, "/users/user/name");
         if (nodes == null){
             return null;
         }
-        ArrayList <String> dicts;
-        dicts = new ArrayList(nodes.getLength());
+        ArrayList <String> users;
+        users = new ArrayList(nodes.getLength());
         for (int i=0;i<nodes.getLength();i++){
-            dicts.add(nodes.item(i).getTextContent());
+            users.add(nodes.item(i).getTextContent());
         }
-        return dicts;
+        return users;
     }
     
-    /* Return -1 if already exist the dictionary with "dict" name, else
-     * return 0, and create the item in XML map file and the XML dict file.
+    /* Return -1 if already exist the user with "user" name, else
+     * return 0, and create the item in XML map file and the XML userDB file.
      */
     public int createUser (String user){
         if (!existUser(user) && !user.equals("")){
-            /* CREATION XML: If dict does not exist is launched this catch, creates dict and return 0 
-             * Same process of createXMLDictNameMapping, see above. */
+            /* CREATION XML: If user does not exist is launched this catch, creates user and return 0 */
             createEmptyUserDB(user);
-            /* ADDING ITEM XMLMAP: Take the root element "dicts" (first item, index 0) with xpath */
-            addDictItemMap(user);
-            /* If this is the first dictionary set as CURRENT_DICT_DOC */
-            NodeList countDict = getNodeListFromDoc(XML_MAP_DOC, "/users/user");
-            if (countDict.getLength() == 1)
-                loadDict(user);
+            /* ADDING ITEM XMLMAP: Take the root element "users" (first item, index 0) with xpath */
+            addUserItemMap(user);
+            /* If this is the first user set as CURRENT_PLAINDB_DOC */
+            NodeList countUsers = getNodeListFromDoc(XML_MAP_DOC, "/users/user");
+            if (countUsers.getLength() == 1)
+                loadUser(user);
             /* SUCCESS */
             return 0;
         } else {
             System.out.println("DEBUGGING: createUser, user already exist or empty name, return -1");
-            /* If dict already exist, it will not be created */
+            /* If user already exist, it will not be created */
             return -1;
         }
     }
     
-    /* Return -1 if does not exist the dictionary with "dict" name, else
-     * return 0, and remove the item in XML map file and the XML dict file.
+    /* Return -1 if does not exist the user with "user" name, else
+     * return 0, and remove the item in XML map file and the XML userDB file.
      */
     public int removeUser (String user){
         if (existUser(user)) {
-            // TODO: Set the next dictionary in XML, and not only set null value
+            // TODO: Set the next user in XML, and not only set null value
             if (user.equals(CURRENT_PLAINDB_NAME)) {
                 CURRENT_PLAINDB_NAME = "";
             }
-            removeDictionary(user);
-            /* ADDING ITEM XMLMAP: Take the root element "dicts" (first item, index 0) with xpath */
-            removeDictItemMap(user);
+            removeUserDB(user);
+            /* ADDING ITEM XMLMAP: Take the root element "users" (first item, index 0) with xpath */
+            removeUserItemMap(user);
             return 0;
         } else {
-            System.out.println("removeUser: removedict, dict does not exist, return -1");
+            System.out.println("removeUser: removeuser, user does not exist, return -1");
             return -1;
         }
     }
     
-    /**** PRIVATE UTILS ****/
-    /* Creates the initial XML File with associations dictionaryName-dictionaryFile */ // TODO Maybe is not necessary, taking directly the user(name/db)
+    /****************************************** PRIVATE UTILS ******************************************/
+    /* Creates the initial XML File with associations user-userDB */
     private void createMappingUserFile (String xmlMapFilename){
         try {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
@@ -218,13 +239,13 @@ public class SingletonRequests {
         }
     }
     
-    /* Check if dict is an item in the XML Map file */
+    /* Check if user is an item in the XML Map file */
     private boolean existUser (String user){
         NodeList nodes = getNodeListFromDoc(XML_MAP_DOC, "/users/user/name/[text() = '"+user+"']");
         return (nodes != null)? true:false;
     }
     
-    /* Create an empty dictionary XML file */
+    /* Create an empty userDB XML file */
     private void createEmptyUserDB (String user) {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = null;
@@ -239,47 +260,47 @@ public class SingletonRequests {
         
         saveDocChanges(doc, user+".xml");
 
-        System.out.println("New dictionary "+user+".xml saved!");
+        System.out.println("New user "+user+".xml saved!");
     }
     
-    /* Add an item in XML Map File for dict */
-    private void addDictItemMap(String dict) {
+    /* Add an item in XML Map File for user */
+    private void addUserItemMap(String user) {
 
-        NodeList nodes = getNodeListFromDoc(XML_MAP_DOC, "/dicts");
+        NodeList nodes = getNodeListFromDoc(XML_MAP_DOC, "/users");
         
-        /* Create and append the new dict element */
-        Element newDict = XML_MAP_DOC.createElement("dict");
+        /* Create and append the new user element */
+        Element newUser = XML_MAP_DOC.createElement("user");
 
-        Element dictName = XML_MAP_DOC.createElement("name");
-        Text textName = XML_MAP_DOC.createTextNode(dict);
-        dictName.appendChild(textName);
+        Element userName = XML_MAP_DOC.createElement("name");
+        Text textName = XML_MAP_DOC.createTextNode(user);
+        userName.appendChild(textName);
 
-        Element dictFile = XML_MAP_DOC.createElement("file");
-        Text textFile = XML_MAP_DOC.createTextNode(dict+".xml");
-        dictFile.appendChild(textFile);
+        Element userFileDB = XML_MAP_DOC.createElement("file");
+        Text textFile = XML_MAP_DOC.createTextNode(user+".xml");
+        userFileDB.appendChild(textFile);
 
-        newDict.appendChild(dictName);
-        newDict.appendChild(dictFile);
-        nodes.item(0).appendChild(newDict);
+        newUser.appendChild(userName);
+        newUser.appendChild(userFileDB);
+        nodes.item(0).appendChild(newUser);
 
         saveDocChanges(XML_MAP_DOC, Configurations.pathOfMainFolder+Configurations.fileNameUsersXML);
         System.out.println("DEBUGGING: createMappingNameXMLFile, Saved changes in XML Map file!");    
     }
     
-    /* Remove the XML file "dict".xml */
-    private void removeDictionary(String dict){
-        File f = new File(dict+".xml");
+    /* Remove the XML file "user".xml */
+    private void removeUserDB(String user){
+        File f = new File(user+".xml");
         try{
             f.delete();
         } catch(Error err){
             System.out.println("DEBUGGING: removeXMLFile, error on delete: "+err);
         }
-        System.out.println("DEBUGGING: removeXMLFile, file "+dict+".xml removed!");
+        System.out.println("DEBUGGING: removeXMLFile, file "+user+".xml removed!");
     }
 
-    private void removeDictItemMap(String dict){
+    private void removeUserItemMap(String user){
 
-        NodeList node2remList = getNodeListFromDoc(XML_MAP_DOC, "/dicts/dict[name[text() = '"+dict+"']]");
+        NodeList node2remList = getNodeListFromDoc(XML_MAP_DOC, "/users/user[name[text() = '"+user+"']]");
 
         /* Go back to parent and remove myself */
         node2remList.item(0).getParentNode().removeChild(node2remList.item(0));
